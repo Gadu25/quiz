e5d1a5976f879d1cb4812b9b000cd73b7853dc1e
unknown <alexanderudag@gmail.com>
2024-09-17 10:24:49 +0800

    update v2

--- js/moment.js
+++ js/moment.js
@@ -4,7 +4,7 @@ let dateNow = moment()
 
 const japanTimeString = dateNow.format('YYYY ねん M げつ D にち HH じ mm ふん ss びょう');
 
-const londonTimeString = dateNow.format('DD.MM YYYY | HH:mm:ss');
+const londonTimeString = dateNow.format('DD.MM YYYY | HH: mm: ss');
 
 console.log("Date Now:", dateNow.format('YYYY MM DD | HH:mm:ss'))
 console.log('Converted to Japan Time:', japanTimeString);
